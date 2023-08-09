3eee90a11543eb126f075cd7dc6300e33f185480
Gmixz <ros.biomed@gmail.com>
2023-08-08 23:02:14 -0400

    SideBar and Main Content Section

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorials',
+        productNumber: '854743',
+        paymentStatus: 'Due',
+        Status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        Status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        Status: 'Active'
+    },
+]
