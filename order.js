99260ae51e691da464ea65ba05b64640c85607e6
Gmixz <ros.biomed@gmail.com>
2023-08-21 21:26:22 -0400

    Added Analyses Section. Added Analyse CSS style. Added New Users Section. Added New Users CSS style. Added Recent Orders Table section. Added Recent Orders Table CSS style. Added Right Section. Added Right CSS style. Added Order and Dark Mode logic.

--- order.js
+++ order.js
@@ -3,18 +3,18 @@ const Orders = [
         productName: 'JavaScript Tutorials',
         productNumber: '854743',
         paymentStatus: 'Due',
-        Status: 'Pending'
+        status: 'Pending'
     },
     {
         productName: 'CSS Full Course',
         productNumber: '97245',
         paymentStatus: 'Refunded',
-        Status: 'Declined'
+        status: 'Declined'
     },
     {
         productName: 'Flex-Box Tutorial',
         productNumber: '36452',
         paymentStatus: 'Paid',
-        Status: 'Active'
+        status: 'Active'
     },
 ]
